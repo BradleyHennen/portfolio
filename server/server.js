1447f90c4785475ff97cd92c65b185c457032f9b
Bradley Hennen <bradhennen5@gmail.com>
2019-06-18 15:05:14 -0500

    moved index

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./public/'));
+app.use(express.static('./'));
 
 app.listen(port, function () {
     console.log('Listening on port: ', port);
